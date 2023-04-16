dba5035494f217446479a568ae462698ef01f187
jerry <jerryfan71@gmail.com>
2023-04-16 12:20:21 -0700

    finish search bar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import "./SearchBar.css";
+
+export const SearchBar = ({ setResults }) => {
+  const [input, setInput] = useState("");
+
+  // create a function that will fetch the external API
+  const fetchData = (value) => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => {
+        // console.log(json)
+        const result = json.filter((user) => {
+          return (
+            value &&
+            user &&
+            user.name &&
+            user.name.toLowerCase().includes(value)
+          );
+        });
+        setResults(result);
+      });
+  };
+
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
+
+  return (
+    <div className="input-wrapper">
+      <FaSearch id="search-icons" />
+      <input
+        placeholder="Type to Search..."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+    </div>
+  );
+};
